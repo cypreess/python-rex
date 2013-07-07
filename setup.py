d85519cf0ef13bebb7931c266773433130b7f4ab
Krzysztof Dorosz <cypreess@gmail.com>
2013-07-08 00:35:09 +0200

    Bumping version to 0.3

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@ from setuptools import setup
 
 setup(
     name='python-rex',
-    version='0.2',
+    version='0.3',
     packages=[],
     py_modules=['rex'],
-    url='',
+    url='https://github.com/cypreess/python-rex',
     license='MIT',
     author='Krzysztof Dorosz',
     author_email='cypreess@gmail.com',
-    description='Python regular expressions for humans'
+    description='Python regular expressions for humans',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    )
 )
